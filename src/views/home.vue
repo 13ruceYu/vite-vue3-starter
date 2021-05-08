0e12a4c748db8ca5512b0555615f158a002135ac
13ruceYu <bruceyuhongbo@mail.com>
2021-05-08 16:30:01 +0800

    chore: actions test

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,16 @@
 <template>
-  <div></div>
+  <div class="home-container page-container">
+    <img class="vue-element-plus-logo" alt="Vue logo" src="../assets/logo.png" />
+    <div class="page-title">Vite2.x + Vue3.x + TypeScript + Element Plus</div>
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   name: 'Home'
-}
+})
 </script>
 
-<style></style>
+<style scoped lang="stylus"></style>
